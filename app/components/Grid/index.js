4d9e7461d372fec171d2d768f0f1a1e15a50e6df
Cjacobson1986 <chrisjacobson1986@gmail.com>
2017-08-17 13:24:04 -0400

    hotkeys to change BG color

--- app/components/Grid/index.js
+++ app/components/Grid/index.js
@@ -17,24 +17,54 @@ export default class Grid extends React.PureComponent {
   constructor() {
     super();
     this.state = {
-      images: ['alcovewaterfall.jpg','Ashvillewaterfall.jpg','bluewaterfall.jpg','bumpywaterfall.jpg','cliffwaterfall.jpg','tsunami.jpg','waterfallBGfucia.jpg','purplewaterfall.jpg',]
+      images: ['alcovewaterfall.jpg','Ashvillewaterfall.jpg','bluewaterfall.jpg','bumpywaterfall.jpg','cliffwaterfall.jpg','tsunami.jpg','waterfallBGfucia.jpg','purplewaterfall.jpg',],
+      Username: "",
+      backgroundColor: "blue"
     }
-  }
-  render() {
-    return (
-              <div>
-            <div className="grid">
-              {this.state.images.map((image,index) =>(
-                <div className= "gridItem" key={index}>
-                  <img src={require('../../images/'+image)}
-                    className="gridImage" />
-                </div>
-              ))}
 
-              </div>
+  }
+  inputUsername = (event) => {
+    this.setState({
+    Username:event.target.value
+    })
+    console.log(Username);
+  }
+
+  storeData = (color) => {
+    console.log (color);
+  }
+
+  changeColor=(color) => {
+    this.setState({
+      backgroundColor:color
+    })
+  }
+
+  render() {
+
+
+
+    return (
+      <div style={{background:this.state.backgroundColor}}>
+        <div className="grid">
+          {this.state.images.map((image,index) =>(
+            <div className= "gridItem" key={index}>
+              <img src={require('../../images/'+image)}
+                className="gridImage" />
             </div>
+          ))}
+
+        </div>
+        <input type="password" onChange={this.inputUsername} placeholder="Username"/>
+        <input type="submit"  onClick={()=> this.changeColor("red")} value="Angry"/>
+        <input type="submit" onClick={()=> this.changeColor("blue")} value="Peaceful"/>
+        <input type="submit" onClick={()=> this.changeColor("black")} value="Sad"/>
+
+      </div>
+
     );
   }
+
 }
 
 Grid.contextTypes = {
